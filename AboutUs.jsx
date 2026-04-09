b57576982f5e1029b18072116d61374d8bc66b14
AndresRiosDev <rios31556@gmail.com>
2026-04-09 12:02:51 -0500

    Create AboutUs.jsx

---
+++ AboutUs.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function AboutUs() {
+  return (
+    <div className="about-us-container">
+      <h1 className="about-us-title">About Us</h1>
+      <p className="about-us-description">Welcome to Paradise Nursery, where green dreams come true!</p>
+      <p className="about-us-content">
+        We are passionate about bringing nature closer to your home. Our nursery offers a wide
+        variety of plants, from indoor succulents to majestic garden trees.
+      </p>
+    </div>
+  );
+}
+
+export default AboutUs;
